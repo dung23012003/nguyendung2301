892064295da22d010b89ef313af1e84d61419105
dung23012003 <nguyendung23012003@gmail.com>
2024-01-17 21:16:30 +0700

    init project

---
+++ Lab02Servlet/src/main/java/devmaster/edu/vn/servlet/HelloServlet.java
@@ -0,0 +1,61 @@
+package devmaster.edu.vn.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ServletOutputStream out= response.getOutputStream();
+		out.println("<html");
+		out.println("<head><title>HelloServlet</title></head>");
+		out.println("<body>");
+		out.println("<h1> helloword </h1>");
+		out.println("<p>This is my fisrt servlet</p>");
+		out.println("<p>Input Phone :");
+		out.println(" <input type='text' name='phone'/>");
+		out.println("</p> <input type='submit' value='Search'/>");
+		out.println("</form>");
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ bt1/src/main/java/bt1/bt123.java
@@ -0,0 +1,10 @@
+package bt1;
+
+public class bt123 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
